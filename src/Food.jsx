24407a5ed25ed69092c8e2164018cd192557503f
futurebound <mitchellm1618@gmail.com>
2024-06-04 15:28:46 -0700

    feat: Food component

---
+++ src/Food.jsx
@@ -0,0 +1,14 @@
+function Food() {
+  return (
+    <>
+      <p>You know I just really love apples and peanut butter</p>
+      <img
+        src='https://www.lottaveg.com/wp-content/uploads/2017/05/Apple-Peanut-Butter.jpg'
+        alt='apple'
+        width='600px'
+      />
+    </>
+  );
+}
+
+export default Food;
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import Greeting from './Greeting.jsx';
+import Food from './Food.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Greeting />
+    <Food />
   </React.StrictMode>
 );

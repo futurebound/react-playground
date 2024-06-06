41472de1240293f948dfeacfcb4054cf72e28622
futurebound <mitchellm1618@gmail.com>
2024-06-06 15:55:52 -0700

    feat: PackingList conditional rendering of importance
    goal to show multiple { } JSX statements in a single element

--- src/PackingList.jsx
+++ src/PackingList.jsx
@@ -1,5 +1,10 @@
-function Item({ name, isPacked }) {
-  return <li className='item'>{isPacked ? name + '✔' : name}</li>;
+function Item({ name, isPacked, importance }) {
+  return (
+    <li className='item'>
+      {isPacked ? name + '✔' : name}
+      {importance > 0 ? <em> (Importance: {importance})</em> : ''}
+    </li>
+  );
 }
 
 export default function PackingList() {
@@ -7,9 +12,9 @@ export default function PackingList() {
     <section>
       <h1>Sally Ride&apos;s Packing List</h1>
       <ul>
-        <Item isPacked={true} name='Space suit' />
-        <Item isPacked={true} name='Helmet with a golden leaf' />
-        <Item isPacked={false} name='Photo of Tam' />
+        <Item isPacked={true} importance={9} name='Space suit' />
+        <Item isPacked={true} importance={0} name='Helmet with a golden leaf' />
+        <Item isPacked={false} importance={6} name='Photo of Tam' />
       </ul>
     </section>
   );

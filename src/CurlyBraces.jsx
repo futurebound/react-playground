f9d9eedcc9543023ce871975b9d3887184de3c3e
futurebound <mitchellm1618@gmail.com>
2024-06-06 13:47:17 -0700

    feat(curly-braces): passing objects in JSX curly braces

--- src/CurlyBraces.jsx
+++ src/CurlyBraces.jsx
@@ -2,5 +2,15 @@ export default function Avatar() {
   const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
   const description = 'Gregorio Y. Zara';
 
-  return <img className='avatar' src={avatar} alt={description} />;
+  return (
+    <img
+      style={{
+        backgroundColor: 'black',
+        color: 'pink',
+      }}
+      className='avatar'
+      src={avatar}
+      alt={description}
+    />
+  );
 }

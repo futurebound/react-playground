f3266b228922922f2e4e2914bda7e40cae1041cb
futurebound <mitchellm1618@gmail.com>
2024-06-13 22:41:55 -0700

    fix: People typo fix

--- src/People.jsx
+++ src/People.jsx
@@ -3,7 +3,7 @@ import { getImageUrl } from './utils.js';
 
 export default function List() {
   const nonChemists = people.filter((person) => person.profession !== 'chemist');
-  const listItems = chemists.map((person) => (
+  const listItems = nonChemists.map((person) => (
     <li key={person.id}>
       <img src={getImageUrl(person)} alt={person.name} />
       <p>

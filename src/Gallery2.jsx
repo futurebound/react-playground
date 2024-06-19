e8a177c6fe13c9c88f92b2cc055a58ff40d22f18
futurebound <mitchellm1618@gmail.com>
2024-06-19 15:50:45 -0700

    fix: Gallery2 handle index wrapping

--- src/Gallery2.jsx
+++ src/Gallery2.jsx
@@ -6,7 +6,11 @@ export default function Gallery() {
   const [showMore, setShowMore] = useState(false);
 
   function handleNextClick() {
-    setIndex(index + 1);
+		if (index < 11) {
+			setIndex(index + 1);
+		} else {
+			setIndex(0)
+		}
   }
 
   function handleMoreClick() {
